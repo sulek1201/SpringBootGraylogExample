1ccd989a07da810b47b2e926497163d89704b23e
sulek1201 <aanil1201@gmail.com>
2022-01-06 22:19:19 +0300

    stateProvince eklendi

--- demo/src/main/java/com/javaworkshop/demo/dtos/ResponseDto.java
+++ demo/src/main/java/com/javaworkshop/demo/dtos/ResponseDto.java
@@ -1,5 +1,6 @@
 package com.javaworkshop.demo.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -9,6 +10,8 @@ public class ResponseDto {
     private String country;
     private String name;
     private String alpha_two_code;
+    @JsonProperty("state-province")
+    private String stateProvince;
     private List<String> domains;
     private List<String> web_pages;
 
