9f68508a8b0b24afd348646628ddb2fa83833f77
sulek1201 <aanil1201@gmail.com>
2022-06-09 16:41:03 +0300

    graylog eklendi

--- demo/src/main/java/com/javaworkshop/demo/DemoApplication.java
+++ demo/src/main/java/com/javaworkshop/demo/DemoApplication.java
@@ -1,15 +1,20 @@
 package com.javaworkshop.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class DemoApplication {
 	public static void main(String[] args) {
+		Logger logger = LogManager.getLogger(DemoApplication.class);
+		logger.info("Application Start Running...");
+		logger.debug("Application Start Running...");
+		logger.error("Application Start Running...");
+		logger.warn("Application Start Running...");
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
--- demo/src/main/java/com/javaworkshop/demo/service/UniversityServiceImpl.java
+++ demo/src/main/java/com/javaworkshop/demo/service/UniversityServiceImpl.java
@@ -1,6 +1,8 @@
 package com.javaworkshop.demo.service;
 
 import com.javaworkshop.demo.dtos.ResponseDto;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -12,11 +14,15 @@ import java.util.List;
 
 @Service
 public class UniversityServiceImpl implements  UniversityService {
+
+    Logger logger = LogManager.getLogger(UniversityServiceImpl.class);
+
     @Autowired
     RestTemplate restTemplate;
 
     @Override
     public List<ResponseDto> getUniversityList(String countryName) {
+        logger.info("getUniversityList is running with: " + countryName);
         final String uri = "http://universities.hipolabs.com/search?country="+countryName;
 
         ResponseEntity<List<ResponseDto>> responseEntity =
@@ -27,6 +33,8 @@ public class UniversityServiceImpl implements  UniversityService {
                         new ParameterizedTypeReference<List<ResponseDto>>() {
                         }
                 );
+
+        logger.info("response: " + responseEntity.toString());
         return responseEntity.getBody();
     }
 }
